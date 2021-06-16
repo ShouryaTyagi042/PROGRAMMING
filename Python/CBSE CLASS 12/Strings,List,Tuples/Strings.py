7cafbae3a7ab1b0c2196d1003aac6b29ea3d3f7e
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2021-06-16 08:37:32 +0530

    Update Strings.py

---
+++ Python/CBSE CLASS 12/Strings,List,Tuples/Strings.py
@@ -0,0 +1,24 @@
+
+# print(type(s))
+# print(s[-4:])
+
+# l1 = [1,2,3]
+# l1[3] = 4
+# print(l1)
+# print(l1+2)
+d = {"a":"science","b":"maths","c":"cs"}
+# for i in d :
+#     print(i,"\t",d[i])
+a = d.get("a")
+print(a)
+x = d.items()
+print(x)
+y = list(d.keys())
+print(y)
+
+s = ("shourya tyagi")
+print(s.lstrip("gi"))
+
+n = "2"
+m = "6"
+print(type(eval(n+m)))
