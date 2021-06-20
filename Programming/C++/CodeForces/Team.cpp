7b6787af21cb4c422fa0b7737640d2b15ab42754
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2021-06-20 22:34:27 +0530

    CodeForces

---
+++ Programming/C++/CodeForces/Team.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std ;
+
+int main() {
+    // int ques,ans ;
+    // cin >> ques ;
+    // ans = 0 ;
+    // for (int i = 0 ; i < ques ; i++ ) {
+    //     int a,b,c ;
+    //     scanf("%d %d %d",&a,&b,&c) ;
+    //     if (a+b+c >= 2) {
+    //         ans++ ;
+    //     }
+    // cout << ans ;
+    // return 0 ;
+    int a,b,c ;
+    scanf("%d %d %d",&a,&b,&c) ;
+    cout << a+b+c << endl ;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    return 0 ;
+}
--- Programming/C++/CodeForces/Watermelon.cpp
+++ Programming/C++/CodeForces/Watermelon.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     int x ;
     cin >> x ;
-    if (x%2==0) {
-        cout << "YES" ;
+    if (x%2==0 && x != 2) {
+        cout << "YES" << endl ;
     }
     else {
-        cout << "NO" ;
+        cout << "NO" << endl ;
     }
 
 
