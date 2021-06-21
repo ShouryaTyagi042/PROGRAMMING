260fdb6cbe540d7f951940bdfe75ab981d04ed0d
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2021-06-21 11:08:13 +0530

    CodeForces

--- Programming/C++/CodeForces/Team.cpp
+++ Programming/C++/CodeForces/Team.cpp
@@ -3,33 +3,32 @@
 using namespace std ;
 
 int main() {
-    // int ques,ans ;
-    // cin >> ques ;
-    // ans = 0 ;
-    // for (int i = 0 ; i < ques ; i++ ) {
-    //     int a,b,c ;
-    //     scanf("%d %d %d",&a,&b,&c) ;
-    //     if (a+b+c >= 2) {
-    //         ans++ ;
-    //     }
-    // cout << ans ;
-    // return 0 ;
-    int a,b,c ;
-    scanf("%d %d %d",&a,&b,&c) ;
-    cout << a+b+c << endl ;
-
-
-
+    int ques,ans ;
+    cin >> ques ;
+    ans = 0 ;
+    for (int i = 0 ; i < ques ; i++ ) {
+        int a,b,c ;
+        scanf("%d %d %d",&a,&b,&c) ;
+        if (a+b+c >= 2) {
+            ans++ ;
+        }
     }
-
-
-
-
-
-
-
-
+    cout << ans ;
 
 
     return 0 ;
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
