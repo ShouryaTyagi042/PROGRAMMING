1b1a64771726fc48c9ce0f5214959d7a9d859383
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2021-06-21 21:23:33 +0530

    CodeForces

---
+++ C++/CodeForces/OrdinaryNumbers.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std ;
+
+
+int main() {
+    int x ;
+    cin >> x ;
+    cout << x << endl ;
+    for (int i = 0 ; i<=x ; i++) {
+        int a,ans ;
+        cin >> a >> endl ;
+        cout << a << endl ;
+    // cout << ans << endl  ;
+
+        // for (int k = 0 ; k < a ; k++) {
+        //     string str1 ;
+        //     str1 = to_string(k) ;
+        //     bool flag = true ;
+        //     int l = str1.length() ;
+        //     for (int j = 2 ; j < l ; j++ ) {
+        //         if (str1[0] != str1[j]) {
+        //             flag = false ;
+        //             break;
+        //         }
+        //     if (flag == true) {
+        //         ans ++ ;
+        //     }
+
+        //     }
+        // cout << ans << endl ;
+        return 0 ;
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
