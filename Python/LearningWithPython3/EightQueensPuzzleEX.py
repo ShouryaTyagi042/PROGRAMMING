7631438919311763a7651e382d0f21291c470da4
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2021-12-19 23:12:55 +0530

    Update EightQueens Puzzle

--- Python/LearningWithPython3/EightQueensPuzzleEX.py
+++ Python/LearningWithPython3/EightQueensPuzzleEX.py
@@ -1,3 +1,7 @@
+import math
+import random
+rng = random.Random()
+
 def share_diagonal(x0, y0, x1, y1):
     """ Is (x0, y0) on a shared diagonal with (x1, y1)? """
     dy = abs(y1 - y0)        # Calc the absolute y distance
@@ -22,9 +26,7 @@ def has_clashes(the_board):
             return True
     return False
 def main():
-    import random
-    rng = random.Random()   # Instantiate a generator
-
+   # Instantiate a generator
     bd = list(range(8))     # Generate the initial permutation
     num_found = 0
     tries = 0
@@ -35,5 +37,55 @@ def main():
            print("Found solution {0} in {1} tries.".format(bd, tries))
            tries = 0
            num_found += 1
+l = list(range(1,50))
+def lotto() :
+    lotto = random.sample(l,6)
+    return lotto
+# print(lotto())
+l1 = lotto()
+key = [2,8,40,47,15,34]
+def lotto_match(l1,l2) :
+    l1.sort()
+    l2.sort()
+    xi = 0
+    yi =0
+    result = []
+    while True :
+        if xi >= 6 or yi >= 6 :
+            break
+        elif l1[xi] < l2[yi] :
+            xi += 1
+        elif l1[xi] > l2[yi] :
+            yi += 1
+        else :
+            result.append(l1[xi])
+            xi += 1
+    return result
+# print(lotto_match(l1,key))
+def is_prime(s) :
+    flag = True
+    if s == 1 :
+        flag = False
+    else :
+        end = math.ceil(math.sqrt(s))
+        for i in range(2,end+1) :
+            if s%i == 0 :
+                flag = False
+                break
+        return flag
+# print(is_prime(4))
 
-main()
+def primes_in(l1) :
+    ans = 0
+    for num in l1 :
+        if is_prime(num) == True :
+            ans += 1
+    return ans
+# print(primes_in([42, 4, 7, 11, 1, 13]))
+
+
+
+
+
+
+
