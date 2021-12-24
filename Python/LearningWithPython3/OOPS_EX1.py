480f89d0e2514d1571747c285e732ea4df06da01
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2021-12-25 00:12:39 +0530

    SMS_Store Class

--- Python/LearningWithPython3/OOPS_EX1.py
+++ Python/LearningWithPython3/OOPS_EX1.py
@@ -35,9 +35,64 @@ class Point :
 # print(p1.slope_from_origin())
 # # print(Point.slope_from_origin(p1)) Two ways to call the method
 # print(p1.equation_of_line(p2))
-
+list_of_messages = []
 class SMS_store :
-    def __init__(self,)
+    def __init__(self,has_been_viewed=False, from_number="NA", time_arrived="NA", text_of_SMS="NA") :
+        self.has_been_viewed = has_been_viewed
+        self.from_number = from_number
+        self.time_arrived = time_arrived
+        self.text_of_SMS = text_of_SMS
+
+    def add_new_arrival(self,from_number, time_arrived, text_of_SMS) :
+        message = (False,from_number, time_arrived, text_of_SMS)
+        list_of_messages.append(message)
+
+    def message_count(self) :
+        return len(list_of_messages)
+
+    def get_unread_message(self) :
+        unread = []
+        for i in list_of_messages :
+            if i[0] == False :
+                unread.append(list_of_messages.index(i))
+        return unread
+
+    def get_message(self,i) :
+        if i >= len(list_of_messages) :
+            return None
+        else :
+            message_unread = list_of_messages[i]
+            message_read = (True,message_unread[1],message_unread[2],message_unread[3])
+            list_of_messages.pop(i)
+            list_of_messages.insert(i,message_read)
+            return message_read
+
+
+    def clear(self) :
+        list_of_messages.clear()
+
+    def delete(self,i) :
+        list_of_messages.pop(i)
+
+my_inbox = SMS_store()
+# print(my_inbox.text_of_SMS)
+my_inbox.add_new_arrival("9871955895","13:00","Hey There I am using Whatsapp")
+my_inbox.add_new_arrival("8527787467","13:00","Hey There I am Rajni Tyagi")
+my_inbox.add_new_arrival("8800798964","13:00","Hey There I am Shourya Tyagi")
+my_inbox.add_new_arrival("7838346721","13:00","Hey There I am Jai Bhagwan Tyagi")
+
+print(my_inbox.message_count())
+print(my_inbox.get_unread_message())
+print(my_inbox.get_message(2))
+print(my_inbox.get_unread_message())
+# print(list_of_messages)
+
+
+
+
+
+
+
 
 
 
