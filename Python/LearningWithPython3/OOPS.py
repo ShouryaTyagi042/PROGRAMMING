3e6d0e495f11497c5849169621eb8c2162368d01
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2021-12-25 23:38:02 +0530

    Update  OOPS

--- Python/LearningWithPython3/OOPS.py
+++ Python/LearningWithPython3/OOPS.py
@@ -1,24 +1,6 @@
 from math import sqrt
-class Point:
-    """ Point class represents and manipulates x,y coords. """
-    def __init__(self):
-
-        """ Create a new point at the origin """
-        self.x = 0
-        self.y = 0
-
-p = Point()
-q = Point()
-print(p.x,p.y)
-print(q.x,q.y)
-
-p.x = 4
-p.y = 5
-
-print("(x={0},y={1})".format(p.x,p.y))
-# distance_from_origin = math.sqrt(p.x*p.x + p.y*p.y)
-
-class APoint :
+from UnitTesting import test
+class Point :
     def __init__(self,x=0,y=0) :
         self.x = x
         self.y = y
@@ -32,16 +14,54 @@ class APoint :
     def __str__(self) :
         return"({},{})".format(self.x,self.y)
 
+class Rectangle:
+    """ A class to manufacture rectangle objects """
+
+    def __init__(self, posn, w, h):
+        """ Initialize rectangle at posn, with width w, height h """
+        self.corner = posn
+        self.width = w
+        self.height = h
+
+    def __str__(self):
+        return  "({0}, {1}, {2})".format(self.corner, self.width, self.height)
+    def area (self) :
+        return self.width*self.height
+    def perimeter(self) :
+        return 2*(self.width + self.height)
+    def flip(self) :
+        width = self.width
+        self.width = self.height
+        self.height = width
+    def contains(self,target) :
+        if self.corner.x < target.y < (self.corner.x + self.width) and (self.corner.y - self.height) < target.y < self.corner.y :
+            return True
+
+
+box = Rectangle(Point(0, 0), 100, 200)
+bomb = Rectangle(Point(100, 80), 5, 10)    # In my video game
+print("box: ", box)
+print("bomb: ", bomb)
+print(bomb.area())
+print(box.perimeter())
+print(box)
+box.flip()
+print(box)
+
+r = Rectangle(Point(0, 0), 10, 5)
+test(r.contains(Point(0, 0)))
+test(r.contains(Point(3, 3)))
+test(not r.contains(Point(3, 7)))
+test(not r.contains(Point(3, 5)))
+test(r.contains(Point(3, 4.99999)))
+test(not r.contains(Point(-3, -3)))
 
 
 
 
 
-p1 = APoint(3,4)
-print(p1.distance_from_origin())
-p1.print_point()
-str(p1)
-print(p1)
+
+
 
 
 
