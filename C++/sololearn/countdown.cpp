d066e45f5988af32c0e63e46a235591cb4ccc2f6
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2021-12-27 12:08:40 +0530

    c++

--- C++/sololearn/countdown.cpp
+++ C++/sololearn/countdown.cpp
@@ -1,21 +1,41 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+
+//     int n;
+//     cin >> n;
+//     // int a = 0
+
+//     //your code goes here
+//     for (int a = n ; a >= 1 ; a-- ) {
+//         cout << a  << endl ;
+//         if ( a % 5 == 0) {
+//             cout << "Beep" << endl ;
+//         }
+
+
+//     }
+
+//     return 0;
+// }
+#include <iostream>
 using namespace std;
 
 int main() {
-
     int n;
     cin >> n;
-    // int a = 0
-
-    //your code goes here
-    for (int a = n ; a >= 1 ; a-- ) {
-        cout << a  << endl ;
-        if ( a % 5 == 0) {
+    int a = n ;
+    while (a<=1) {
+        cout << a << endl ;
+        if (a%5 == 0) {
             cout << "Beep" << endl ;
         }
-
+        a-- ;
 
     }
+
+
 
     return 0;
 }
