1580af8e76c64d4bff3f894fa3ca19fefd674126
ShouryaTyagi042 <81747739+ShouryaTyagi042@users.noreply.github.com>
2022-01-18 17:48:57 +0530

    mac

---
+++ C++/mac.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std ;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+    freopen("output.in", "w", stdout);
+#endif
+    int n ;
+    cin >> n ;
+    cout << n ;
+}
---
+++ Python/MAC_Python.py
@@ -0,0 +1,4 @@
+number = 87
+print(number)
+for num in range(5):
+    print(num)
