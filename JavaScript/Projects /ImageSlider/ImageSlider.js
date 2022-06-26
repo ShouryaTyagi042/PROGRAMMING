8082209d2ffee00f017afa3430a555e0e15587e8
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-06-26 18:32:02 +0530

    createdjs

---
+++ JavaScript/Projects /ImageSlider/ImageSlider.js
@@ -0,0 +1,25 @@
+var images = [
+    /Users/shouryatyagi/Desktop/PROGRAMMING/JavaScript/Projects /ImageSlider/Img1.jpeg,
+    /Users/shouryatyagi/Desktop/PROGRAMMING/JavaScript/Projects /ImageSlider/Img2.jpeg,
+    /Users/shouryatyagi/Desktop/PROGRAMMING/JavaScript/Projects /ImageSlider/Img3.jpeg
+] ;
+
+var num = 0 ;
+function next() {
+    var slider = document.getElementById(slider);
+    num ++ ;
+    if (num >= images.length) {
+        num = 0 ;
+    }
+    slider.src = images[num] ;
+}
+
+function prev() {
+    var slider = document.getElementById('slider');
+    num--;
+    if(num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
+}
+
