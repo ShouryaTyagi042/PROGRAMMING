e425ce9917801e6042ed61babcaa7d68c7b9ac7c
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-11-03 22:51:25 +0530

    added images

--- Python/DIP Workshop/Starting.py
+++ Python/DIP Workshop/Starting.py
@@ -3,7 +3,7 @@ import cv2
 
 # To read image from disk, we use
 # cv2.imread function, in below method,
-img = cv2.imread("geeksforgeeks.png", cv2.IMREAD_COLOR)
+img = cv2.imread("images/GingerFresca.jpg", cv2.IMREAD_COLOR)
 
 # Creating GUI window to display an image on screen
 # first Parameter is windows title (should be in string format)
@@ -21,4 +21,3 @@ cv2.waitKey(0)
 # It is for removing/deleting created GUI window from screen
 # and memory
 cv2.destroyAllWindows()
-
