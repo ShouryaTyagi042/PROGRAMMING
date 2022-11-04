7b3026ead1e96a7627414e9358cdcc13e9acbd3e
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-11-04 21:31:47 +0530

    opencv intro

--- Python/DIP Workshop/Starting.py
+++ Python/DIP Workshop/Starting.py
@@ -1,23 +1,16 @@
-# Python code to read image
-import cv2
+# importing required libraries
+import matplotlib.pyplot as plt
+import matplotlib.image as img
 
-# To read image from disk, we use
-# cv2.imread function, in below method,
-img = cv2.imread("images/GingerFresca.jpg", cv2.IMREAD_COLOR)
+# reading the image
+testImage = img.imread('Python/DIP Workshop/GingerFresca.jpg')
 
-# Creating GUI window to display an image on screen
-# first Parameter is windows title (should be in string format)
-# Second Parameter is image array
-cv2.imshow("image", img)
+# displaying the shape of the image
+# print(testImage.shape)
 
-# To hold the window on screen, we use cv2.waitKey method
-# Once it detected the close input, it will release the control
-# To the next line
-# First Parameter is for holding screen for specified milliseconds
-# It should be positive integer. If 0 pass an parameter, then it will
-# hold the screen until user close it.
-cv2.waitKey(0)
+# modifying the shape of the image
 
-# It is for removing/deleting created GUI window from screen
-# and memory
-cv2.destroyAllWindows()
+
+# displaying the modified image
+plt.imshow(testImage)
+plt.show()
