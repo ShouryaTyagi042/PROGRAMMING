55499520f16e96df32ce44e15a46abf61333b012
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-11-30 21:09:48 +0530

    CNDY Problem

---
+++ C++/CodeChef/CNDY.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) ((a > b) ? b : a)
+// #define mod 1e9 + 7
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+// #define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
+// #define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
+#define INF 1000000000000000003
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+void solve()
+{
+    int l;
+    cin >> l;
+    l *= 2;
+    int arr[l];
+    unordered_map<int, int> m;
+    FOR(i, l)
+    {
+        cin >> arr[i];
+        m[arr[i]]++;
+    }
+    FOR(i, l)
+    {
+        // cout << m[arr[i]] << " ";
+        if (m[arr[i]] > 2)
+        {
+            cout << "NO"
+                 << "\n";
+            return;
+        }
+    }
+    cout << "YES"
+         << "\n";
+    return;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int T;
+    cin >> T;
+    while (T > 0)
+    {
+        solve();
+
+        T--;
+    }
+    return 0;
+}
