e823a4dc6b1cb410ef2d510d12ee71e76f06a170
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-01-03 21:43:35 +0530

    Started with Dart

--- Dart/First.dart
+++ Dart/First.dart
@@ -1,3 +1,10 @@
+void printInteger(int number) {
+  print('The number given is $number');
+}
+
 void main() {
-  print('Hello World!');
+  var number = 78;
+  printInteger(number);
+  const n = 8;
+  print(n);
 }
