a03c390e18c431eb83990dbb8be43c94b151ab62
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-06-01 23:15:18 +0530

    Update check_1.py

--- PythonCheckFiles/check_1.py
+++ PythonCheckFiles/check_1.py
@@ -1,13 +1,34 @@
-def count(sub,st) :
-    step = 0
-    c = 0
-    l = len(sub)
-    while True :
-        s = st[step:].find(sub)
-        if s != -1 :
-            c += 1
-            step += s + 1
-        else :
-            break
-    return c
-print(count("aaa", "aaaaaa"))
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+class BST:
+    def __init__(self) :
+        self.root = None
+    def inorder(self,root):
+        if root:
+            self.inorder(root.left)
+            print(root.val, end=" ")
+            self.inorder(root.right)
+    def insert(self,root, key):
+        if root is None:
+            return Node(key)
+        else:
+            if root.val == key:
+                return root
+            elif root.val < key:
+                root.right = self.insert(root.right, key)
+            else:
+                root.left = self.insert(root.left, key)
+        return root
+
+root=BST();
+
+root.insert(15);
+root.insert(10);
+root.insert(23);
+root.insert(6);
+root.insert(20);
+root.insert(30);
+
