dc9ee6167e3dba3b7baf8e2df0d64b8acf727976
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-09-13 18:13:23 +0530

    added the code files for dip workshop

---
+++ Python/DIP Workshop/ImageOperation/imageOperations.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+
+# loading the image
+img = cv2.imread("Python/DIP Workshop/ImageOperation/FerrariImage.jpg")
+
+
+# # Accessing the image properties
+# print(img.shape)
+# print(img.size)
+# print(img.dtype)
+
+
+# Accessing the pixel
+cv2.imshow("original", img)
+px = img[100, 100]
+print(px)
+
+# img[100, 100] = [255, 0, 0]
+# updatedPx = img[100, 100]
+# print(updatedPx)
+# img[0:200, 0:400] = [255, 0, 0]
+
+
+# Setting the font and font color
+# font = cv2.FONT_HERSHEY_SIMPLEX
+# white = (255, 255, 255)
+# cv2.putText(img, "Red Color Image",
+#             (100, 100), font, 4, white, 4)
+# cv2.imshow("updated", img)
+# (name_image, text,coordinates for the text,
+# font_type, font_scale,color,thickness)
+
+
+# Cropping the image
+croppedImage = img[0:600, 0:600]
+cv2.imshow("Cropped Image", croppedImage)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
